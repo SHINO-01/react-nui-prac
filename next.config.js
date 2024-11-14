1a18f2a4f7be90253108de663bc3ad084fe5ac2a
SHINO-01 <sakifhussain33@gmail.com>
2024-11-14 18:52:57 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+module.exports = {
+    webpack(config) {
+      config.resolve.extensions.push('.ts', '.tsx');
+      return config;
+    }
+  };
